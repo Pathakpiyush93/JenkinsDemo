06d7643d79867f9c0833fc21c0196d6f6c58057a
Pathakpiyush93 <pathakpiyush93@gmail.com>
2024-04-25 12:17:14 +0530

    Parameterized browser

--- src/test/java/com/jenkins/JenkinsDemo/Demo_For_Jenkins.java
+++ src/test/java/com/jenkins/JenkinsDemo/Demo_For_Jenkins.java
@@ -2,17 +2,28 @@ package com.jenkins.JenkinsDemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo_For_Jenkins {
 	
+	@Parameters("Browser")
 	@Test
-	public void demo() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+	public void demo(String browserName) {
+		WebDriver driver=null;
+		if(browserName.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		else if(browserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
+		
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match.");
 //		driver.close();
